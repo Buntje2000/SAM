b9baefbfc8ff58fb3f57eb43567fced3ce271db2
rubenvanderschaft <45173251+rubenvanderschaft@users.noreply.github.com>
2022-04-19 16:53:35 +0200

    logging configuration

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
+import logging
 import time
+
 from app.services.pipeline import start
 
+logging.basicConfig(filename='test.log', level=logging.DEBUG,
+                    format='%(asctime)s:%(levelname)s:%(message)s')
 
 start_time = time.time()
 
 # Start pipeline
 start()
 
-print("--- Looptijd: %s seconden ---" % round(time.time() - start_time, 3))
+logging.info("--- Looptijd: %s seconden ---" % round(time.time() - start_time, 3))
